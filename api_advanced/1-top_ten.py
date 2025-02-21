504750a42110260d78bafe6b2062dcac2d884e01
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-02-21 03:51:51 -0800

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
 """
-This module queries the Reddit API and prints the titles of the
-top ten hot posts of a given subreddit. Prints None if the subreddit is invalid.
+Query Reddit API for titles of top ten posts of a given subreddit
 """
-
 import requests
 
 
 def top_ten(subreddit):
     """
-    Print the titles of the top ten hot posts for a given subreddit.
-    Print None if the subreddit is invalid.
+        return top ten titles for a given subreddit
+        return None if invalid subreddit given
     """
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/yourusername)'
-    }
-
-    # Make the request without following redirects
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is valid
-    if response.status_code != 200:
+    r = requests.get(url, headers=headers).json()
+    top_ten = r.get('data', {}).get('children', [])
+    if not top_ten:
         print(None)
-        return
-
-    posts = response.json().get('data', {}).get('children', [])
-
-    if not posts:
-        print(None)
-        return
-
-    for post in posts:
-        print(post.get('data', {}).get('title'))
+    for t in top_ten:
+        print(t.get('data').get('title'))
