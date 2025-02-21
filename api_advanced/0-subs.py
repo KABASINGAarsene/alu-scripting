890d11ca77b7040643611e7d70a35e75074bfe53
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-02-21 03:46:13 -0800

    Task

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,24 +2,27 @@
 """
 Query Reddit API for number of subscribers for a given subreddit
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-        return number of subscribers for a given subreddit
-        return 0 if invalid subreddit given
+    Return number of subscribers for a given subreddit.
+    Return 0 if an invalid subreddit is given.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
+    # Set custom User-Agent to avoid rate limiting
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    # Prevent following redirects and handle status codes properly
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if request is successful
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
         return 0
-    return subscribers
