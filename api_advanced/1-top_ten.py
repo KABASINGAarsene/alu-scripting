39f4b36153e55c899f9e5c674d99d17c7f090f66
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-02-21 03:48:16 -0800

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,24 +2,32 @@
 """
 Query Reddit API for titles of top ten posts of a given subreddit
 """
+
 import requests
 
 
 def top_ten(subreddit):
     """
-        return top ten titles for a given subreddit
-        return None if invalid subreddit given
+    Print the titles of the top ten hot posts for a given subreddit.
+    Print None if an invalid subreddit is given.
     """
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    r = requests.get(url, headers=headers).json()
-    top_ten = r.get('data', {}).get('children', [])
-    if not top_ten:
+
+    # Check response status before calling .json()
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
-    for t in top_ten:
-        print(t.get('data').get('title'))
+        return
+
+    data = response.json().get('data', {}).get('children', [])
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post.get('data', {}).get('title'))
+
